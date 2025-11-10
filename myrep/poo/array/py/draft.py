a2e3ca6a7fa3f619a42b22eff4d1b1cdc88c4c21
Vinicius <viniciusvje47@gmail.com>
2025-11-10 09:02:30 -0300

    Terminei Array

--- myrep/poo/array/py/draft.py
+++ myrep/poo/array/py/draft.py
@@ -1,3 +1,4 @@
+import random
 class Foo:
     def __init__(self, x: int):
         self.x = x
@@ -33,6 +34,52 @@ print(lista_preenchida)
 listaString = [str(x) for x in lista_preenchida]
 listaForm = " ".join(listaString)
 print(listaForm)
+
+lista_vazia_int = list(range(8))
+print(lista_vazia_int)
+
+lista_alearorios = [random.randint(1,100) for i in range(5)]
+print(lista_alearorios)
+
+print(lista_preenchida[0])
+print(lista_preenchida[2])
+
+for i in lista_preenchida:
+    print(i)
+
+for i in range(len(lista_preenchida)):
+    print(lista_preenchida[i])
+
+for x in lista_preenchida:
+    if x == 3:
+        print("Numero 3 encotrado")
+    else:
+        print("Numero 3 não encontrado")
+
+if 3 in lista_preenchida:
+    print("Numero encontrado")
+
+newList = [x for x in lista_preenchida if x % 2 == 0]
+print(newList)
+
+newList1 = [ x ** 2 for x in lista_preenchida]
+print(newList1)
+
+lista_preenchida.remove(3)
+print(lista_preenchida)
+
+newLista3 = [1,2,3,3,3,4,4,5,5,6,1,2,9,10,3,3,3]
+while 3 in newLista3:
+    newLista3.remove(3)
+print(newLista3)
+
+newList4 = [5, 3, 2, 7, 1, 4, 6]
+newList4.sort()
+print(newList4)
+
+newList7 = [1,2,3,4,5,6,7,8,9,10]
+random.shuffle(newList7)
+print(newList7)
 
 
 
